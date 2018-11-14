f034bd8a200180fc6ad0e135c603c03a337c4589
Stephanie Douglas <stephanietdouglas@gmail.com>
2018-11-14 13:37:00 -0500

    measured Halpha for all Hyades MDM spectra (including some individual exposures)

--- fit_halpha.py
+++ fit_halpha.py
@@ -18,21 +18,24 @@ if __name__=="__main__":
              ("eqw_med","f8"),("eqw16","f8"), ("eqw84","f8")]
     tab = Table(np.zeros(len(files),dtype=dtype))
 
-    mdm_path = os.path.expanduser("~/Dropbox/data/MDM_Hyades/")
+#    mdm_path = os.path.expanduser("~/Dropbox/data/MDM_Hyades/")
+    mdm_path = os.path.expanduser("/pool/cfsi04_0/data/MDM_Hyades/")
     for i,filename in enumerate(files):
         ssplit = filename.split("/")
         name = ssplit[-1][:-12]
         print(name)
+        tab["filename"][i] = filename
 
         if os.path.exists(filename):
-            filebase = os.path.join(mdm_path,"figures/{0}.png".format(name))
+#            filebase = os.path.join(mdm_path,"figures/{0}.png".format(name))
+            filebase = "figures/{0}.png".format(name)
             eperc = EqW.measure_equivalent_width(str(filename),
                                 6550,6580,6560,6566,
                                 1000,xunit="Angstrom",to_plot=True,
                                 filebase=filebase)
             tab["eqw16"][i], tab["eqw_med"][i], tab["eqw84"][i] = eperc
-        if i>10:
-            break
+#        if i>3:
+#            break
 
     # fmt = {"eqw_med":"%.4f",
     #         "eqw16":"%.4f",
